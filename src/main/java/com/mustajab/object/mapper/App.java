44a07b14e5e2bf1640ec8b2185d925877166b104
Mustajab Akhtar <Mustajab.Akhtar@makemytrip.com>
2017-03-23 20:53:19 +0530

    Initial commit

---
+++ src/main/java/com/mustajab/object/mapper/App.java
@@ -0,0 +1,13 @@
+package com.mustajab.object.mapper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
